cae03c368a8d94c01900e562e0af4ff2047467d2
Mukul Sahu <mukulcoding9090@gmail.com>
2026-07-08 11:28:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (22.27%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int copyx=x;
+        int rev=0;
+        while(x>0){
+            int digit=x%10;
+            if(rev>INT_MAX/10){
+                return false;
+            }
+            rev=(rev*10)+digit;
+            x/=10;
+        }
+        return rev==copyx;;
+        
+    }
+};
