22a02cf392fa7cd2494159a78c782c224e1e2b8e
Mukul Sahu <mukulcoding9090@gmail.com>
2026-07-12 12:51:34 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (66.08%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
@@ -4,15 +4,15 @@ public:
         //optimal code 
         int ans=0;
         for(int i=0;i<nums.size();i++){
-        if(nums[i]<=99 and nums[i]>=10){
-            ans++;
-        }
-        else if(nums[i]<=9999 and nums[i]>=1000){
-            ans++;
-        }
-        else if(nums[i]<=999999 and nums[i]>=100000){
-            ans++;
-        }
+            if(nums[i]<=99 and nums[i]>=10){
+                 ans++;
+            }
+            else if(nums[i]<=9999 and nums[i]>=1000){
+                ans++;
+            }
+            else if(nums[i]<=999999 and nums[i]>=100000){
+                ans++;
+            }
 
         }
         
