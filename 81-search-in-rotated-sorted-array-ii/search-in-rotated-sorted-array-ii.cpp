b6069350352f4c8d41335e1bbd537e1c85654e19
Mukul Sahu <mukulcoding9090@gmail.com>
2026-07-16 12:22:04 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (80.55%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -4,11 +4,16 @@ public:
         int st=0;
         int end=nums.size()-1;
         while(st<=end){
-            int mid=st+(end-st/2);
+            int mid=st+(end-st)/2;
             if(nums[mid]==target){
                 return true;
             }
-            if(nums[st]<nums[mid]){    //left sort
+            if(nums[st]==nums[mid] && nums[mid]==nums[end]){
+                st++;
+                end--;
+                continue;
+            }
+            if(nums[st]<=nums[mid]){    //left sort
                 if(nums[st]<=target && nums[mid]>target){
                     end=mid-1;
                 }
