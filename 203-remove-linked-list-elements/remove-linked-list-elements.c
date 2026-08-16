57447532094fe13093bc08eda8dddf0b3c452d08
Mukul Sahu <mukulcoding9090@gmail.com>
2026-08-16 20:04:04 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (62.37%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.c
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    struct ListNode* ptr;
+        ptr=head;
+        // ptr=(struct ListNode*)malloc(sizeof(struct ListNode));
+        struct ListNode* q;
+        q=(struct ListNode*)malloc(sizeof(struct ListNode));
+        int count=0;
+        while(ptr!=NULL){
+            if(ptr->next!=NULL){
+                q=ptr->next;
+            }
+            count=0;
+            if(ptr->next!=NULL){
+                if(q->val==val){
+                    ptr->next=ptr->next->next;
+                    free(q);
+                    count++;
+                }
+
+            }
+            if(count==0){
+                ptr=ptr->next;
+            }
+
+        }
+        if(head!=NULL){
+            if(head->val==val){
+                q=head;
+                head=head->next;
+                free(q);
+            }
+
+        }
+        
+        return head;
+    
+    
+}
