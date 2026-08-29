06baba5f8b10587422daabdda246b72f09a6545a
Mukul Sahu <mukulcoding9090@gmail.com>
2026-08-29 19:09:59 +0530

    Time: 12 ms (10.82%) | Memory: 31.1 MB (23.64%) - LeetSync

---
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> arr;
+        for(int i=0;i<nums.size();i++){
+            int dig=nums[i]*nums[i];
+            arr.push_back(dig);
+        }
+        sort(arr.begin(),arr.end());
+        return arr;
+        
+    }
+};
